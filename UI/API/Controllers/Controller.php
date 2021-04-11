61d59446a3cf42d7b283c2cbde329677de7c8835
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-04-11 15:26:28 +0430

    Remove usage of apiato caller style and use FQCN instead

--- UI/API/Controllers/Controller.php
+++ UI/API/Controllers/Controller.php
@@ -3,20 +3,22 @@
 namespace App\Containers\VendorSection\Welcome\UI\API\Controllers;
 
 use Apiato\Core\Foundation\Facades\Apiato;
+use App\Containers\VendorSection\Welcome\Actions\FindMessageForApiRootVisitorAction;
+use App\Containers\VendorSection\Welcome\Actions\FindMessageForApiV1VisitorAction;
 use App\Ship\Parents\Controllers\ApiController;
 use Illuminate\Http\JsonResponse;
 
 class Controller extends ApiController
 {
-    public function apiRoot(): JsonResponse
-    {
-        $message = Apiato::call('Welcome@FindMessageForApiRootVisitorAction');
-        return response()->json($message);
-    }
+	public function apiRoot(): JsonResponse
+	{
+		$message = Apiato::call(FindMessageForApiRootVisitorAction::class);
+		return response()->json($message);
+	}
 
-    public function v1ApiLandingPage(): JsonResponse
-    {
-        $message = Apiato::call('Welcome@FindMessageForApiV1VisitorAction');
-        return response()->json($message);
-    }
+	public function v1ApiLandingPage(): JsonResponse
+	{
+		$message = Apiato::call(FindMessageForApiV1VisitorAction::class);
+		return response()->json($message);
+	}
 }
